ceb0e8eef7095a3072c1deac5a5910679f4db023
Kiran Bhatta <bhattakiran1111@gmail.com>
2025-08-04 07:45:20 +0545

    Create Lab_1_4_using_command_line_convert_OC_to_OF.java
    Write a program to convert Oc to Of using Command line input.

---
+++ Lab_1_4_using_command_line_convert_OC_to_OF.java
@@ -0,0 +1,13 @@
+public class TemperatureConvert{
+  public static void main(String[] args){
+if(args.lenth!=1){
+  System.out.println("usage: java Temperature convert to Celsius");
+   reture;
+}
+    double celsius =double.parseDouble(args[0]);
+    souble fahrenheit =(celsius*9/5)+32);
+     System.out.println("Temperature in faharenheit:" + fahrenheit);
+      }
+    }
+      
+  
