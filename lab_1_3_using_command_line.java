c6ff75b43eb51aef96a63726442443baa6a1afcf
Kiran Bhatta <bhattakiran1111@gmail.com>
2025-08-04 07:34:04 +0545

    Create lab_1_3_using_command_line.java
    Program to find area and perimeter of rectangle input are taken from command line.

---
+++ lab_1_3_using_command_line.java
@@ -0,0 +1,15 @@
+public class rectangle{
+  public static void main(String[] args){
+if(args.length !=2)
+{
+  System.out.println("Usage: java rectangle<length><witdth>");
+  return;
+  {
+double length=double.parseDouble(args[0]);
+    double width=double.parseDouble(args[1]);
+    double area =lenth*width;
+    double perimeter =2*(length+width);
+    System.out.println("Area:"+area);
+    System.out.println("perimeter:"+perimeter);
+            }
+            }
