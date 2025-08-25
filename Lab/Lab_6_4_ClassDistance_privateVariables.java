ae43ee3bbb6abb31809c1a7f1c3793a50d868d8a
Kiran Bhatta <bhattakiran1111@gmail.com>
2025-08-26 01:10:01 +0545

    Update Lab_6_4_ClassDistance_privateVariables.java
    Write class distance containing private variables feet int & inches of type int suitable constructors and two methods add distance and display distance for adding and display distance objects. Write a separate class My Distance contains main method to create and distance objects.

--- Lab/Lab_6_4_ClassDistance_privateVariables.java
+++ Lab/Lab_6_4_ClassDistance_privateVariables.java
@@ -1,16 +1,30 @@
-class Distance{
-  private int feet;
-  private int inches;
-  feet=0;
-  inches=0;
+class Distance {
+    private int feet;
+    private int inches;
+    Distance(int f, int i) {
+        feet = f;
+        inches = i;
+        normalize();
+    }
+    void normalize() {
+        feet += inches / 12;
+        inches = inches % 12;
+    }
+    Distance addDistance(Distance d) {
+        int totalFeet = this.feet + d.feet;
+        int totalInches = this.inches + d.inches;
+        return new Distance(totalFeet, totalInches);
+    }
+    void displayDistance() {
+        System.out.println(feet + " Feet " + inches + " Inches");
+    }
 }
-Distance (int f,int i){
-  feet =f;
-  inches =i;
-  normalize();
+public class MyDistance {
+    public static void main(String[] args) {
+        Distance d1 = new Distance(5, 8);
+        Distance d2 = new Distance(3, 11);
+        Distance d3 = d1.addDistance(d2);
+        System.out.println("Total Distance:");
+        d3.displayDistance();
+    }
 }
-void normalize(){
-  feet =inches/12;
-  inches =inches%12;
-}
-Distance add
