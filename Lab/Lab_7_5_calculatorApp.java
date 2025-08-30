853d0abe7932a6e4a7425516ddb1cca117f6baf4
Kiran Bhatta <bhattakiran1111@gmail.com>
2025-08-30 08:24:01 +0545

    Create Lab_7_5_calculatorApp.java

---
+++ Lab/Lab_7_5_calculatorApp.java
@@ -0,0 +1,40 @@
+interface AddSubtraction {
+    int add(int a, int b);
+    int subtraction(int a, int b);
+}
+
+interface MultiplyDivide {
+    int multiply(int a, int b);
+    double divide(int a, int b);
+}
+
+class Calculator implements AddSubtraction, MultiplyDivide {
+
+    public int add(int a, int b) {
+        return a + b;
+    }
+
+    public int subtraction(int a, int b) {
+        return a - b;
+    }
+
+    public int multiply(int a, int b) {
+        return a * b;
+    }
+
+    public double divide(int a, int b) {
+        if (b == 0) {
+            System.out.println("Cannot divide by zero.");
+            return 0;
+        }
+        return (double) a / b;
+    }
+
+    public static void main(String[] args) {
+        Calculator calc = new Calculator();
+        System.out.println("Addition: " + calc.add(10, 5));
+        System.out.println("Subtraction: " + calc.subtraction(10, 5));
+        System.out.println("Multiplication: " + calc.multiply(10, 5));
+        System.out.println("Division: " + calc.divide(10, 5));
+    }
+}
